9f8cfeabd70dfa70d800f244f95ff6402d3bf293
Yisaaaa <ryansanisit19@gmail.com>
2024-03-16 10:12:29 +0800

    removed files related to redux since were not using it anymore in this main branch

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Provider } from "react-redux";
-import store from "./store";
 import NotificationContextProvider from "./contexts/NotificationContextProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import UserContextProvider from "./contexts/UserContextProvider";
@@ -10,12 +8,10 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<QueryClientProvider client={queryClient}>
-		<Provider store={store}>
-			<UserContextProvider>
-				<NotificationContextProvider>
-					<App />
-				</NotificationContextProvider>
-			</UserContextProvider>
-		</Provider>
+		<UserContextProvider>
+			<NotificationContextProvider>
+				<App />
+			</NotificationContextProvider>
+		</UserContextProvider>
 	</QueryClientProvider>
 );
